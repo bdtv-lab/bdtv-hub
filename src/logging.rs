ee7b243d20190083075499957f224f99e5b56e43
peter2500zz <peter2500zz@qq.com>
2026-08-24 19:16:48 +0800

    chore: 添加协议

--- src/logging.rs
+++ src/logging.rs
@@ -5,12 +5,11 @@ const OWN_TARGET: &str = env!("CARGO_CRATE_NAME");
 
 pub fn init(printer: Printer) {
     let filter = EnvFilter::builder()
-        .with_default_directive(tracing::Level::TRACE.into())
+        .with_default_directive(tracing::Level::WARN.into())
         .parse_lossy(&format!("{}=trace", OWN_TARGET));
 
     tracing_subscriber::fmt()
-        // .with_env_filter(filter)
-        .with_max_level(tracing::Level::TRACE)
+        .with_env_filter(filter)
         .with_writer(move || printer.clone())
         .init();
 }
