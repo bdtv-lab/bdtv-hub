3256d880eca8f4bfa08a434469adeb6c9f9ce91e
peter2500zz <peter2500zz@qq.com>
2026-08-28 11:53:41 +0800

    feat: http 部分的 server 查询接口改为返回 object

--- src/server/goto.rs
+++ src/server/goto.rs
@@ -1,11 +1,17 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use axum::{Json, extract::State};
 
 use crate::{app, types::Server};
 
-pub(super) async fn get_servers(State(state): State<Arc<app::State>>) -> Json<Vec<Server>> {
-    let mut servers: Vec<_> = state.online_servers.lock().await.keys().cloned().collect();
-    servers.sort_by(|a, b| a.slug.cmp(&b.slug));
+pub(super) async fn get_servers(
+    State(state): State<Arc<app::State>>,
+) -> Json<HashMap<String, Server>> {
+    let server_map = state.online_servers.lock().await;
+    let servers = server_map
+        .keys()
+        .into_iter()
+        .map(|server| (server.slug.clone(), server.clone()))
+        .collect();
     Json(servers)
 }
